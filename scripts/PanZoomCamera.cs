d98ecaa513b59c7fcd304758a0731f3122a1442c
jmac-lin-oculo <master.scratchman@gmail.com>
2025-06-19 10:43:58 +0100

    finished that fucking arch curve

--- scripts/PanZoomCamera.cs
+++ scripts/PanZoomCamera.cs
@@ -5,6 +5,6 @@ public partial class PanZoomCamera : Camera3D
 {
     public override void _Process(double delta)
     {
-        GetParent<Node3D>().RotateY((float)delta * 2.0f);
+        GetParent<Node3D>().RotateY((float)delta * 0.2f);
     }
 }
